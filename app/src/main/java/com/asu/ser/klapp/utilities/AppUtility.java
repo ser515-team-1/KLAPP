7233010236be647ea6ebcf394cb99277c8cdf97f
Rohit <r4rohit002@gmail.com>
2019-09-21 17:02:44 -0700

    Created Apputility and init func

---
+++ app/src/main/java/com/asu/ser/klapp/utilities/AppUtility.java
@@ -0,0 +1,14 @@
+package com.asu.ser.klapp.utilities;
+
+import android.content.Context;
+
+public class AppUtility {
+
+    public static Context context;
+
+    /** This method should be called in the Splash Activity**/
+    public static void init(Context _context){
+        context = _context;
+    }
+
+}
