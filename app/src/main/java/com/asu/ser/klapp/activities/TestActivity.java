c263e1255b3d152c422b6f993c6cd650fb84a5bf
Rohit <r4rohit002@gmail.com>
2019-09-22 14:12:50 -0700

    Added Utility Methods for loading and saving User Credentials

--- app/src/main/java/com/asu/ser/klapp/activities/TestActivity.java
+++ app/src/main/java/com/asu/ser/klapp/activities/TestActivity.java
@@ -1,6 +1,7 @@
 package com.asu.ser.klapp.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.asu.ser.klapp.R;
 
--- app/src/main/java/com/asu/ser/klapp/utilities/AppUtility.java
+++ app/src/main/java/com/asu/ser/klapp/utilities/AppUtility.java
@@ -1,6 +1,7 @@
 package com.asu.ser.klapp.utilities;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.asu.ser.klapp.models.Credential;
 
@@ -8,24 +9,39 @@ public class AppUtility {
 
     public static Context context;
 
+    private static final String USER_CRED_PREF = "com.asu.ser.klapp.models.Credential.USER_CRED_PREF";
+    private static SharedPreferences pref_user_credentials;
+    private static SharedPreferences.Editor editor;
+    private static String USERNAME_KEY;
+    private static String PASSWORD_KEY;
+
+
     /** This method should be called in the Splash Activity**/
     public static void init(Context _context){
         context = _context;
+        init_sharedpref();
     }
 
     private static void init_sharedpref(){
 
-    }
-
-    public void saveCredential(Credential credential){
+        pref_user_credentials = context.getSharedPreferences(USER_CRED_PREF, Context.MODE_PRIVATE);
 
     }
 
-    public Credential getCredential(){
+    public static void saveCredential(Credential credential){
 
-        Credential credential = null;
+        editor = pref_user_credentials.edit();
+        editor.putString(USERNAME_KEY, credential.username);
+        editor.putString(PASSWORD_KEY, credential.password);
+        editor.commit();
 
+    }
 
+    public static Credential getCredential(){
+
+        Credential credential = new Credential();
+        credential.setUsername(pref_user_credentials.getString(USERNAME_KEY, "default"));
+        credential.setPassword(pref_user_credentials.getString(PASSWORD_KEY, "default"));
         return credential;
     }
 
