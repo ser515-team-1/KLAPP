ceb4d04c0658b35f08a17b4c7482284d5e6bf91d
Rohit <r4rohit002@gmail.com>
2019-09-24 10:35:46 -0700

    Removed unused imports

--- app/src/main/java/com/asu/ser/klapp/MainActivity.java
+++ app/src/main/java/com/asu/ser/klapp/MainActivity.java
@@ -1,10 +1,6 @@
 package com.asu.ser.klapp;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.asu.ser.klapp.models.Credential;
 import com.asu.ser.klapp.utilities.AppUtility;
 
 import androidx.appcompat.app.AppCompatActivity;
