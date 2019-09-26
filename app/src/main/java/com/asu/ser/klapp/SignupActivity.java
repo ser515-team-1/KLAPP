e1076b18a154823d04276d1cd0f760795a6a2969
surya-ra <ess.dee3107@gmail.com>
2019-09-26 12:16:11 -0700

    Created Signup Activity and layout

---
+++ app/src/main/java/com/asu/ser/klapp/SignupActivity.java
@@ -0,0 +1,17 @@
+package com.asu.ser.klapp;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle sis){
+        super.onCreate(sis);
+        setContentView(R.layout.activity_signup);
+    }
+
+}
