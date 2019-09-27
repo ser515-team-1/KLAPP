b43ba0c8f74204b2764a80a646862b8a9a124203
Rohit <r4rohit002@gmail.com>
2019-09-27 12:40:33 -0700

    Reverted changes in MainActivity

--- app/src/main/java/com/asu/ser/klapp/MainActivity.java
+++ app/src/main/java/com/asu/ser/klapp/MainActivity.java
@@ -1,19 +1,16 @@
 package com.asu.ser.klapp;
 
 import android.os.Bundle;
-import com.asu.ser.klapp.utilities.AppUtility;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppUtility.init(this);
+
     }
 
 }
