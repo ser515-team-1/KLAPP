9cf751ababe5fef5f8593907a3888bb958b3ffff
Ashwath Reddy <rashwath55@gmail.com>
2019-10-07 18:47:43 -0700

    made some changes to the validation function

--- app/src/main/java/com/asu/ser/klapp/activities/BodmasActivity.java
+++ app/src/main/java/com/asu/ser/klapp/activities/BodmasActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.asu.ser.klapp.R;
+
 public class BodmasActivity extends AppCompatActivity {
 
     @Override
