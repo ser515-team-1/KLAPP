03e6832fdc846e73813c6231cccb478b300d82aa
Rohit <r4rohit002@gmail.com>
2019-10-07 20:04:32 -0700

    Created Counting Activity

---
+++ app/src/main/java/com/asu/ser/klapp/activities/CountingActivity.java
@@ -0,0 +1,17 @@
+package com.asu.ser.klapp.activities;
+
+import android.os.Bundle;
+
+import com.asu.ser.klapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CountingActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counting);
+    }
+
+}
