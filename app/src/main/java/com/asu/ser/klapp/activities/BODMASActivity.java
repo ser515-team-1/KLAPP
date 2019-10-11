ddd484b252af6b4b300d8491ca49666882491d9f
Ashwath Reddy <rashwath55@gmail.com>
2019-10-10 23:21:57 -0700

    made changes to code

--- app/src/main/java/com/asu/ser/klapp/activities/BODMASActivity.java
+++ app/src/main/java/com/asu/ser/klapp/activities/BODMASActivity.java
@@ -20,20 +20,29 @@ public class BODMASActivity extends AppCompatActivity {
 
     public static boolean isValidExpression(String str)
     {
-
+        int i=0,k=0;
         str = str.replaceAll("\\s+", "");
-        if (isOperator(str.charAt(0)) || isOperator(str.charAt(str.length()-1)))
-            return false;
+        if (isOperator(str.charAt(0))|| isOperator(str.charAt(str.length()-1)))
+        {
+            if(str.charAt(0)=='-')
+            {
+                i++;
+            }
+            else
+                return false;
+        }
         int count = 0;
 
-        for (int i = 0; i < str.length(); i++)
+        while (i < str.length())
         {
             if (!isOperator(str.charAt(i)) &&!Character.isDigit(str.charAt(i))&&str.charAt(i) != '('&&str.charAt(i) != ')')
                 return false;
             if (str.charAt(i) == '(')
             {
                 count++;
-                if (i == str.length()-1)
+                if(str.charAt(i+1)=='-')
+                    k=1;
+                if (i == str.length()-1||(i!=0&&!isOperator(str.charAt(i-1))))
                     return false;
             }
             if (str.charAt(i) == ')')
@@ -51,17 +60,18 @@ public class BODMASActivity extends AppCompatActivity {
             if (isOperator(str.charAt(i))){
 
 
-                if (str.charAt(i-1) == '(' || str.charAt(i+1) == ')' || isOperator(str.charAt(i+1)))
+                if ((str.charAt(i-1) == '('&&str.charAt(i)!='-') || str.charAt(i+1) == ')' ||isOperator(str.charAt(i-1))|| isOperator(str.charAt(i+1)))
                 {
+
                     return false;
+
                 }
             }
+            i++;
         }
-
-        if  (count== 0)
+        if (count== 0)
             return true;
         else
             return false;
 
     }
-}
