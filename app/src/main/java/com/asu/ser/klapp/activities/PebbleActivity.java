1d22c17e4817cfd5157153df30f22f7e3fecdb08
Rohit <r4rohit002@gmail.com>
2019-10-30 14:50:49 -0700

    Added PebbleActivity

---
+++ app/src/main/java/com/asu/ser/klapp/activities/PebbleActivity.java
@@ -0,0 +1,15 @@
+package com.asu.ser.klapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PebbleActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
