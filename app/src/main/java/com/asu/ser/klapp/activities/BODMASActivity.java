e1e57ce385f39b6149560a533f0e0eccab9e1277
Rohit <r4rohit002@gmail.com>
2019-11-02 21:54:50 -0700

    Added eof

--- app/src/main/java/com/asu/ser/klapp/activities/BODMASActivity.java
+++ app/src/main/java/com/asu/ser/klapp/activities/BODMASActivity.java
@@ -13,55 +13,46 @@ public class BODMASActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bodmas);
     }
-    public static boolean isOperator(char a )
-    {
-        return (a =='*'|| a == '/' || a == '+'|| a == '-');
+
+    public static boolean isOperator(char a) {
+        return (a == '*' || a == '/' || a == '+' || a == '-');
     }
 
-    public static boolean isValidExpression(String str)
-    {
-        int i=0,k=0;
+    public static boolean isValidExpression(String str) {
+        int i = 0, k = 0;
         str = str.replaceAll("\\s+", "");
-        if (isOperator(str.charAt(0))|| isOperator(str.charAt(str.length()-1)))
-        {
-            if(str.charAt(0)=='-')
-            {
+        if (isOperator(str.charAt(0)) || isOperator(str.charAt(str.length() - 1))) {
+            if (str.charAt(0) == '-') {
                 i++;
-            }
-            else
+            } else
                 return false;
         }
         int count = 0;
 
-        while (i < str.length())
-        {
-            if (!isOperator(str.charAt(i)) &&!Character.isDigit(str.charAt(i))&&str.charAt(i) != '('&&str.charAt(i) != ')')
+        while (i < str.length()) {
+            if (!isOperator(str.charAt(i)) && !Character.isDigit(str.charAt(i)) && str.charAt(i) != '(' && str.charAt(i) != ')')
                 return false;
-            if (str.charAt(i) == '(')
-            {
+            if (str.charAt(i) == '(') {
                 count++;
-                if(str.charAt(i+1)=='-')
-                    k=1;
-                if (i == str.length()-1||(i!=0&&!isOperator(str.charAt(i-1))))
+                if (str.charAt(i + 1) == '-')
+                    k = 1;
+                if (i == str.length() - 1 || (i != 0 && !isOperator(str.charAt(i - 1))))
                     return false;
             }
-            if (str.charAt(i) == ')')
-            {
+            if (str.charAt(i) == ')') {
                 count--;
                 if (i == 0)
                     return false;
-                if(i!= str.length()-1)
-                {
-                    if(str.charAt(i+1) != ')'&&!isOperator(str.charAt(i+1)))
+                if (i != str.length() - 1) {
+                    if (str.charAt(i + 1) != ')' && !isOperator(str.charAt(i + 1)))
                         return false;
                 }
 
             }
-            if (isOperator(str.charAt(i))){
+            if (isOperator(str.charAt(i))) {
 
 
-                if ((str.charAt(i-1) == '('&&str.charAt(i)!='-') || str.charAt(i+1) == ')' ||isOperator(str.charAt(i-1))|| isOperator(str.charAt(i+1)))
-                {
+                if ((str.charAt(i - 1) == '(' && str.charAt(i) != '-') || str.charAt(i + 1) == ')' || isOperator(str.charAt(i - 1)) || isOperator(str.charAt(i + 1))) {
 
                     return false;
 
@@ -69,9 +60,11 @@ public class BODMASActivity extends AppCompatActivity {
             }
             i++;
         }
-        if (count==0)
+        if (count == 0)
             return true;
         else
             return false;
 
     }
+
+}
