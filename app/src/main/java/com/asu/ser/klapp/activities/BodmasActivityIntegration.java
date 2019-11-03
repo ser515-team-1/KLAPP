84d1f6962c902b078d39bb824c9b79470707c708
Rohit <r4rohit002@gmail.com>
2019-11-03 02:19:14 -0700

    Integrated UI with activity

---
+++ app/src/main/java/com/asu/ser/klapp/activities/BodmasActivityIntegration.java
@@ -0,0 +1,56 @@
+package com.asu.ser.klapp.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.asu.ser.klapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BodmasActivityIntegration extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText inputField;
+    private Button submitButton;
+    private TextView output;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bodmas_dpolen);
+
+        inputField = findViewById(R.id.inputField);
+        submitButton = findViewById(R.id.submit);
+        output = findViewById(R.id.output);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.submit:
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    private void submit(String input) {
+
+    }
+
+    private void showOutput(){
+
+    }
+
+
+}
