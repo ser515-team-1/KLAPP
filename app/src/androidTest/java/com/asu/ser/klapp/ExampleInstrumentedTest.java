fb9bd43609c1fc30e7edbe6c16bc8a762564be1b
surya-ra <ess.dee3107@gmail.com>
2019-11-06 23:24:00 -0700

    Junit testcases added

--- app/src/androidTest/java/com/asu/ser/klapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/asu/ser/klapp/ExampleInstrumentedTest.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.asu.ser.klapp.activities.BodmasActivity;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -28,7 +30,34 @@ public class ExampleInstrumentedTest {
 
 
     @Test
-    public void testBodmas(){
-         assertTrue(true);
+    public void testBodmas_single(){
+        String exprs_test = "2+3-5";
+        String[] hold_expr_test = exprs_test.split("");
+        BodmasActivity b_obj = new BodmasActivity();
+        String[] hold_operators_test = b_obj.formOperator_arr(hold_expr_test);
+        String o_p = "1,+,3,-";
+        String[] tester =  o_p.split(",");
+        assertEquals(tester, hold_operators_test);
     }
+    @Test
+    public void testBodmas_nested(){
+        String exprs_test = "((2+3)-5)";
+        String[] hold_expr_test = exprs_test.split("");
+        BodmasActivity b_obj = new BodmasActivity();
+        String[] hold_operators_test = b_obj.formOperator_arr(hold_expr_test);
+        String o_p = "1,+,3,-";
+        String[] tester =  o_p.split(",");
+        assertEquals(tester, hold_operators_test);
+    }
+    @Test
+    public void testBodmas_digits(){
+        String exprs_test = "22+31-15";
+        String[] hold_expr_test = exprs_test.split("");
+        BodmasActivity b_obj = new BodmasActivity();
+        String[] hold_operators_test = b_obj.formOperator_arr(hold_expr_test);
+        String o_p = "1,+,3,-";
+        String[] tester =  o_p.split(",");
+        assertEquals(tester, hold_operators_test);
+    }
+
 }
