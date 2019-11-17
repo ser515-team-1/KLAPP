54153484cf31774a76a17b328166144e6531d68d
Ashwath Reddy <rashwath55@gmail.com>
2019-11-17 13:07:37 -0700

    added a method to validate the string in apputility

--- app/src/main/java/com/asu/ser/klapp/utilities/AppUtility.java
+++ app/src/main/java/com/asu/ser/klapp/utilities/AppUtility.java
@@ -164,4 +164,56 @@ public class AppUtility {
         }
         return 0;
     }
+    public static boolean isOperator(char a) {
+        return (a == '*' || a == '/' || a == '+' || a == '-');
+    }
+
+    public static boolean isValidExpression(String str) {
+        int i = 0, k = 0;
+        str = str.replaceAll("\\s+", "");
+        if (isOperator(str.charAt(0)) || isOperator(str.charAt(str.length() - 1))) {
+            if (str.charAt(0) == '-') {
+                i++;
+            } else
+                return false;
+        }
+        int count = 0;
+
+        while (i < str.length()) {
+            if (!isOperator(str.charAt(i)) && !Character.isDigit(str.charAt(i)) && str.charAt(i) != '(' && str.charAt(i) != ')')
+                return false;
+            if (str.charAt(i) == '(') {
+                count++;
+                if (str.charAt(i + 1) == '-')
+                    k = 1;
+                if (i == str.length() - 1 || (i != 0 && !isOperator(str.charAt(i - 1))))
+                    return false;
+            }
+            if (str.charAt(i) == ')') {
+                count--;
+                if (i == 0)
+                    return false;
+                if (i != str.length() - 1) {
+                    if (str.charAt(i + 1) != ')' && !isOperator(str.charAt(i + 1)))
+                        return false;
+                }
+
+            }
+            if (isOperator(str.charAt(i))) {
+
+
+                if ((str.charAt(i - 1) == '(' && str.charAt(i) != '-') || str.charAt(i + 1) == ')' || isOperator(str.charAt(i - 1)) || isOperator(str.charAt(i + 1))) {
+
+                    return false;
+
+                }
+            }
+            i++;
+        }
+        if (count == 0)
+            return true;
+        else
+            return false;
+
+    }
 }
