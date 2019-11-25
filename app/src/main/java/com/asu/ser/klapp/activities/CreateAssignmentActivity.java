887ed414e5787af9944c6f3b4a6bf99d123ac6dd
Rohit <r4rohit002@gmail.com>
2019-11-24 17:40:49 -0700

    Added DatePicker

--- app/src/main/java/com/asu/ser/klapp/activities/CreateAssignmentActivity.java
+++ app/src/main/java/com/asu/ser/klapp/activities/CreateAssignmentActivity.java
@@ -1,11 +1,13 @@
 package com.asu.ser.klapp.activities;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,18 +20,24 @@ import com.asu.ser.klapp.models.Student;
 import com.asu.ser.klapp.utilities.AppUtility;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateAssignmentActivity extends AppCompatActivity implements View.OnClickListener {
+public class CreateAssignmentActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     private EditText left,right;
     private TextView assignmentDetail;
     private Button submit, addMore, finish;
 
+    private EditText dueDate, name;
+
     private Assignment assignment;
     private List<Problem> problemList;
+    final Calendar myCalendar = Calendar.getInstance();
 
     @Override
     public void onCreate(Bundle savedInstaceState){
@@ -44,7 +52,24 @@ public class CreateAssignmentActivity extends AppCompatActivity implements View.
         submit = findViewById(R.id.submit);
         addMore = findViewById(R.id.addMore);
         finish = findViewById(R.id.finish);
-        assignmentDetail = findViewById(R.id.assignmentDetail);
+
+        name = findViewById(R.id.name);
+        dueDate = findViewById(R.id.dueDate);
+
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this, R.style.DialogTheme, CreateAssignmentActivity.this, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
+
+        dueDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                datePickerDialog.show();
+            }
+        });
+
+
 
         addMore.setOnClickListener(this);
         submit.setOnClickListener(this);
@@ -117,4 +142,19 @@ public class CreateAssignmentActivity extends AppCompatActivity implements View.
     }
 
 
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dueDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        myCalendar.set(Calendar.YEAR, year);
+        myCalendar.set(Calendar.MONTH, month);
+        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        updateLabel();
+    }
 }
