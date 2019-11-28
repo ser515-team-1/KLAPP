659f2abc140a4112cda529687f6715f68a8c3c15
surya-ra <ess.dee3107@gmail.com>
2019-11-28 14:33:26 -0700

    This commit will convert the infix equation provided by user into postfix expression

--- app/src/main/java/com/asu/ser/klapp/activities/BodmasActivity.java
+++ app/src/main/java/com/asu/ser/klapp/activities/BodmasActivity.java
@@ -1,5 +1,5 @@
 package com.asu.ser.klapp.activities;
-
+import java.util.Stack;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -290,7 +290,78 @@ public class BodmasActivity extends AppCompatActivity{
         // pair_arr = equation.split("");
         return key_value_vector;
     }
+    /*
+     * This Class converts an infix expression given by user into a postfix expression.
+     *@author: Suryadeep
+     */
+    public class InfixtoPostfix {
+        // A utility function to return precedence of a given operator
+        // Higher returned value means higher precedence
+        int Prec(char ch) {
+            switch (ch) {
+                case '+':
+                case '-':
+                    return 1;
 
+                case '*':
+                case '/':
+                    return 2;
 
+                case '^':
+                    return 3;
+            }
+            return -1;
+        }
 
+        // The main method that converts given infix expression
+        // to postfix expression.
+        String infixToPostfix(String exp) {
+            // initializing empty String for result
+            String result = new String("");
+
+            // initializing empty stack
+            Stack<Character> stack = new Stack<>();
+
+            for (int i = 0; i < exp.length(); ++i) {
+                char c = exp.charAt(i);
+
+                // If the scanned character is an operand, add it to output.
+                if (Character.isLetterOrDigit(c))
+                    result += c;
+
+                    // If the scanned character is an '(', push it to the stack.
+                else if (c == '(')
+                    stack.push(c);
+
+                    //  If the scanned character is an ')', pop and output from the stack
+                    // until an '(' is encountered.
+                else if (c == ')') {
+                    while (!stack.isEmpty() && stack.peek() != '(')
+                        result += stack.pop();
+
+                    if (!stack.isEmpty() && stack.peek() != '(')
+                        return "Invalid Expression"; // invalid expression
+                    else
+                        stack.pop();
+                } else // an operator is encountered
+                {
+                    while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek())) {
+                        if (stack.peek() == '(')
+                            return "Invalid Expression";
+                        result += stack.pop();
+                    }
+                    stack.push(c);
+                }
+
+            }
+
+            // pop all the operators from the stack
+            while (!stack.isEmpty()) {
+                if (stack.peek() == '(')
+                    return "Invalid Expression";
+                result += stack.pop();
+            }
+            return result;
+        }
+    }
 }
