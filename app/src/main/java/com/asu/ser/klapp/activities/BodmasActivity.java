15a48678f579da32b1b6645842574dd8325db04f
surya-ra <ess.dee3107@gmail.com>
2019-11-29 10:28:14 -0700

    this module will help us evaluate the postfix expression

--- app/src/main/java/com/asu/ser/klapp/activities/BodmasActivity.java
+++ app/src/main/java/com/asu/ser/klapp/activities/BodmasActivity.java
@@ -364,4 +364,49 @@ public class BodmasActivity extends AppCompatActivity{
             return result;
         }
     }
+
+    public class eval {
+        // Method to evaluate value of a postfix expression
+         int evaluatePostfix(String exp) {
+            //create a stack
+            Stack<Integer> stack = new Stack<>();
+
+            // Scan all characters one by one
+            for (int i = 0; i < exp.length(); i++) {
+                char c = exp.charAt(i);
+
+                // If the scanned character is an operand (number here),
+                // push it to the stack.
+                if (Character.isDigit(c))
+                    stack.push(c - '0');
+
+                    //  If the scanned character is an operator, pop two
+                    // elements from stack apply the operator
+                else {
+                    int val1 = stack.pop();
+                    int val2 = stack.pop();
+
+                    switch (c) {
+                        case '+':
+                            stack.push(val2 + val1);
+                            break;
+
+                        case '-':
+                            stack.push(val2 - val1);
+                            break;
+
+                        case '/':
+                            stack.push(val2 / val1);
+                            break;
+
+                        case '*':
+                            stack.push(val2 * val1);
+                            break;
+                    }
+                }
+            }
+            return stack.pop();
+        }
+    }
+
 }
