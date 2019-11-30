3931a0b4cadfb373cce83451cc8d35d4c1a1d29e
Khushboo Gupta <khushboogupta.bu@gmail.com>
2019-11-29 19:35:09 -0700

    Assignment.java,added titles

---
+++ app/src/main/java/com/asu/ser/klapp/models/Assignment.java
@@ -0,0 +1,35 @@
+package com.asu.ser.klapp.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  @version         1.0
+ *  date created     11/10/2019
+ */
+public class Assignment implements Serializable {
+
+    private List<Problem> problemList;
+
+    public Assignment(){
+        problemList = new ArrayList<>();
+    }
+
+    public Assignment(List<Problem> problemList){
+        this.problemList = problemList;
+    }
+
+    public void addProbleam(Problem problem){
+        problemList.add(problem);
+    }
+
+    public List<Problem> getProblemList(){
+        return problemList;
+    }
+
+    public void setProblemList(List<Problem> problemList){
+        this.problemList = problemList;
+    }
+
+}
